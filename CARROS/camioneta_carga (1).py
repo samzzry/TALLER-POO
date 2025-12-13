0777c1bdd03458e63d8f4feced5e7a363eed607e
samzzry <samuelhumanez6@gmail.com>
2025-12-12 22:23:44 -0500

    Add files via upload

---
+++ CARROS/camioneta_carga (1).py
@@ -0,0 +1,10 @@
+from carro import Vehiculo
+class CamionetaCarga(Vehiculo):
+    def __init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible, capacidad_carga_kg):
+        Vehiculo.__init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible)
+        self.capacidad_carga_kg = capacidad_carga_kg
+    def climatizacion(self):
+        print("climatizacion basica para cabina de trabajo")
+
+    def info_extra(self):
+        print("capacidad de carga:", self.capacidad_carga_kg, "kg")
---
+++ CARROS/carro_deportivo (1).py
@@ -0,0 +1,9 @@
+from carro import Vehiculo
+class CarroDeportivo(Vehiculo):
+    def __init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible):
+        Vehiculo.__init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible)
+        self.modo_sport = True
+    def aceleracion_frenado(self):
+        print("aceleracion rapida y frenado deportivo")
+    def info_extra(self):
+        print("modo sport activo:", self.modo_sport)
