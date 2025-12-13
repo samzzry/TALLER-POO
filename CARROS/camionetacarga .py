6bc3f7933476d023b7521bb33533b32ea5f3043e
samzzry <samuelhumanez6@gmail.com>
2025-12-12 22:26:21 -0500

    camionetacarga .py

--- CARROS/camionetacarga .py
+++ CARROS/camionetacarga .py
@@ -5,6 +5,5 @@ class CamionetaCarga(Vehiculo):
         self.capacidad_carga_kg = capacidad_carga_kg
     def climatizacion(self):
         print("climatizacion basica para cabina de trabajo")
-
     def info_extra(self):
         print("capacidad de carga:", self.capacidad_carga_kg, "kg")
