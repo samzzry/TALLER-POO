8a4fd1169cc74987a96f58e7815093a7c98a3413
samzzry <samuelhumanez6@gmail.com>
2025-12-15 19:56:46 -0500

    Delete ANIMALES/clase_insecto.py

--- ANIMALES/clase_insecto.py
+++
@@ -1,8 +0,0 @@
-from clase_animal import animal
-class insecto(animal):
-    def __init__(self, nombre, habitat, dieta, reproduccion):
-        animal.__init__(self, nombre, habitat, dieta)
-        self.reproduccion = reproduccion
-    def info(self):
-        print("su reproduccion es:", self.reproduccion)
-        animal.info(self)
