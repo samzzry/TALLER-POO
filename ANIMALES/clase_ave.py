d33fa421d19a56653b3322b0c3f3188874c8fc2a
samzzry <samuelhumanez6@gmail.com>
2025-12-15 19:56:37 -0500

    Delete ANIMALES/clase_ave.py

--- ANIMALES/clase_ave.py
+++
@@ -1,8 +0,0 @@
-from clase_animal import animal
-class ave(animal):
-    def __init__(self, nombre, habitat, dieta, comunicacion):
-        animal.__init__(self, nombre, habitat, dieta)
-        self.comunicacion = comunicacion
-    def info(self):
-        print("su manera de comunicarse es por medio de:", self.comunicacion)
-        animal.info(self)
