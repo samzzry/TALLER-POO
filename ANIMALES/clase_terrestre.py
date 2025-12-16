0a0050c556fcf4fd78b9b30b12899286f6d6aa50
samzzry <samuelhumanez6@gmail.com>
2025-12-15 19:57:02 -0500

    Delete ANIMALES/clase_terrestre.py

--- ANIMALES/clase_terrestre.py
+++
@@ -1,8 +0,0 @@
-from clase_animal import animal
-class terrestre(animal):
-    def __init__(self, nombre, habitat, dieta, color):
-        animal.__init__(self, nombre, habitat, dieta)
-        self.color = color
-    def info(self):
-        print("su pelaje es:", self.color)
-        animal.info(self)
