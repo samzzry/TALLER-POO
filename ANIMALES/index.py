81e38a15a3863bdc8c93ffde161605558d9f5b95
samzzry <samuelhumanez6@gmail.com>
2025-12-16 19:17:10 -0500

    index.py

--- ANIMALES/index.py
+++ ANIMALES/index.py
@@ -1,31 +1,88 @@
+from base_datos import Base_datos
 from modelo_animal_caballo import Caballo
 from modelo_animal_cocodrilo import Cocodrilo
 from modelo_animal_escarabajo import Escarabajo
 from modelo_animal_pescado import Pescado
 from modelo_animal_pato import Pato
 
-caballo = Caballo("Spirit", 5, "Pradera", "Hierba", "Grande", "Marrón", "Mustang", 60)
-caballo.mostrar_info()
-caballo.galopar()
-caballo.alimentarse()
-print()
 
-cocodrilo = Cocodrilo("Coco", 12, "Pantano", "Carnívoro", "Grande", "Verde", 3000)
-cocodrilo.mostrar_info()
-cocodrilo.atacar()
-cocodrilo.nadar()
-print()
+bd = Base_datos()
+bd.cargar_datos_iniciales()
 
-escarabajo = Escarabajo("Bicho", 1, "Bosque", "Hojas", "Pequeño", "Negro", True)
-escarabajo.mostrar_info()
-escarabajo.volar()
-print()
+while True:
+    print("crud animales")
+    print("1. agregar animal")
+    print("2. eliminar animal")
+    print("3. mostrar animales")
+    print("4. salir")
 
-pescado = Pescado("Nemo", 2, "Arrecife", "Plancton", "Pequeño", "Naranja", "Salada")
-pescado.mostrar_info()
-pescado.nadar()
-print()
+    opcion = int(input("seleccione una opcion: "))
 
-pato = Pato("Donald", 3, "Lago", "Omnívoro", "Mediano", "Blanco", "Ancho")
-pato.mostrar_info()
-pato.graznar()
+    match opcion:
+        case 1:
+            tipo = input("tipo (caballo / cocodrilo / escarabajo / pescado / pato): ")
+
+            nombre = input("nombre: ")
+            edad = int(input("edad: "))
+            habitat = input("habitat: ")
+            dieta = input("dieta: ")
+            tamaño = input("tamaño: ")
+            color = input("color: ")
+
+            if tipo == "caballo":
+                raza = input("raza: ")
+                velocidad = int(input("velocidad: "))
+                animal = Caballo(
+                    nombre, edad, habitat, dieta, tamaño, color, raza, velocidad
+                )
+                bd.agregar_animal(animal)
+
+            elif tipo == "cocodrilo":
+                fuerza = int(input("fuerza de mordida: "))
+                animal = Cocodrilo(
+                    nombre, edad, habitat, dieta, tamaño, color, fuerza
+                )
+                bd.agregar_animal(animal)
+
+            elif tipo == "escarabajo":
+                alas = input("tiene alas (si/no): ")
+
+                if alas == "si":
+                    animal = Escarabajo(
+                        nombre, edad, habitat, dieta, tamaño, color, True
+                    )
+                else:
+                    animal = Escarabajo(
+                        nombre, edad, habitat, dieta, tamaño, color, False
+                    )
+
+                bd.agregar_animal(animal)
+
+            elif tipo == "pescado":
+                tipo_agua = input("tipo de agua: ")
+                animal = Pescado(
+                    nombre, edad, habitat, dieta, tamaño, color, tipo_agua
+                )
+                bd.agregar_animal(animal)
+
+            elif tipo == "pato":
+                pico = input("tipo de pico: ")
+                animal = Pato(
+                    nombre, edad, habitat, dieta, tamaño, color, pico
+                )
+                bd.agregar_animal(animal)
+
+            else:
+                print("tipo no valido")
+
+        case 2:
+            posicion = int(input("posicion a eliminar: "))
+            bd.eliminar_por_posicion(posicion)
+
+        case 3:
+            bd.mostrar_lista()
+
+        case 4:
+            break
+
+
